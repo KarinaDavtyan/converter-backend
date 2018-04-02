6dfdd84eff7030296acabf08fbb5df9944043780
KarinaDavtyan <k.davtyan27@gmail.com>
2018-04-02 10:36:13 +0200

    feat: send http statuses with response

--- controllers/converterController.js
+++ controllers/converterController.js
@@ -4,13 +4,16 @@ const realWordCheckerFunctions = require('../helpers/realWordChecker');
 const { converter } = converterFunctions;
 const { realWordChecker } = realWordCheckerFunctions;
 
-
 const convertNumber = async (req, res) => {
   const { text } = req.params;
-  console.log(text);
-  const words = converter(text);
-  const realWords =realWordChecker(words);
-  res.send(realWords);
+  if (text === 'favicon.ico' || !text) {
+    res.sendStatus(400);
+  } else {
+    const words = converter(text);
+    if (words === null) res.sendStatus(400);
+    const realWords = realWordChecker(words);
+    res.status(200).send(realWords);
+  }
 }
 
 module.exports = { convertNumber };
